b94e218c0d6a475862743781b2cc9edab2c039f9
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2023-03-25 11:39:27 +0530

    backend done for now

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,20 @@
-export const getPosts = (req, res) => {
-  res.send("This Works");
+import PostMessage from "../models/postMessage.js";
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(500).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { getPosts } from "../controllers/posts.js";
+import { getPosts, createPost } from "../controllers/posts.js";
 const router = express.Router();
 
 router.get("/", getPosts);
+router.post("/", createPost);
 
 export default router;
