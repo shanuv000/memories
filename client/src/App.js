17eee152d20348f3f947c899a2a3a420db89c458
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2023-03-25 10:14:12 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
+export default App;
---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts";
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://shanuv000:shanu9334187630@cluster0.yd4tco7.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+// // rough
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+// app.get("/users", (req, res) => {
+//   res.send("Users Page");
+// });
+// // rough
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.set("useFindandModify", false);
