707219ef6cb5b9159c7d17da9bd8aae5711a076f
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2023-03-25 10:53:45 +0530

    extra 1

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import postRoutes from "./routes/posts";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,7 @@
 import express from "express";
-
+import { getPosts } from "../controllers/posts.js";
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("This Works");
-});
+router.get("/", getPosts);
 
 export default router;
